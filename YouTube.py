aa458de595c56e321463c49687c889589049c3b3
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2024-02-21 15:30:38 -0400

    Delete YouTube.py

--- YouTube.py
+++
@@ -1,3 +0,0 @@
-#EXTM3U
-#EXTINF:-1 tvg-id="58b2b0a13f424b344e401f10c71981fd" tvg-name="YOUTUBE" tvg-logo="https://logopng.com.br/logos/youtube-54.png" group-title="SHOWS",YOUTUBE 4K
-https://youtu.be/lckPSFj2MPc?si=nmS4dUtKAOkgz8yD
