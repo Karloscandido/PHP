1ef743b1fa32b46e0216a0fac952d5651e75b3b2
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2024-05-17 15:01:37 -0400

    Delete server_encurtado.py

--- server_encurtado.py
+++
@@ -1,9 +0,0 @@
-const options = {
-  method: 'GET',
-  headers: {accept: 'application/json', Authorization: 'sk_OPr7lJv8no8Z1lRm'}
-};
-
-fetch('https://api.short.io/api/links?domain_id=1085622&limit=150&beforeDate=2028-01-01T00%3A00%3A00.000Z&afterDate=2022-01-01T00%3A00%3A00.000Z&dateSortOrder=asc&pageToken=Split_sh', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
