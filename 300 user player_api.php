b9e3a12574c2e921f61bf2ed49ec0eba48f0bc1f
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-03-30 13:58:27 -0400

    Add files via upload

---
+++ 300 user player_api.php
@@ -0,0 +1,40 @@
+{
+  "user_info": {
+    "username": "754710",
+    "password": "pybS26",
+    "username": "268379",
+    "password": "fJ5zEZ",
+    "username": "448807",
+    "password": "PzZeyN",
+    "username": "999476",
+    "password": "yBkSDx",
+    "username": "912220",
+    "password": "EQpVPe",
+    "username": "716942",
+    "password": "VTMRNS",
+    "message": null,
+    "auth": 1,
+    "status": "Active",
+    "exp_date": null,
+    "is_trial": "0",
+    "active_cons": 0,
+    "max_connections": "300",
+    "allowed_output_formats": [
+      "m3u8",
+      "ts",
+      "rtmp"
+    ]
+  },
+  "server_info": {
+    "xui": true,
+    "version": "1.5.5",
+    "revision": 2,
+    "url": "descontraytv.com",
+    "port": "80",
+    "https_port": "443",
+    "server_protocol": "http",
+    "rtmp_port": "8880",
+    "timestamp_now": 1774892165,
+    "timezone": "America/Sao_Paulo"
+  }
+}
