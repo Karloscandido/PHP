448d75a7483fd505ff18cc0c2160a1826f73c894
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-09 22:34:17 -0400

    Update meu tv.php

--- meu tv.php
+++ meu tv.php
@@ -9,7 +9,7 @@
     "is_trial": "0",
     "active_cons": "0",
     "created_at": "1758052661",
-    "max_connections": "20",
+    "max_connections": "200",
     "allowed_output_formats": [
       "m3u8",
       "ts"
