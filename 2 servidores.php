289c33ca4a1e6b77cf13b7f8fad423e857f8bbd0
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-16 20:38:29 -0400

    Update 2 servidores.php

--- 2 servidores.php
+++ 2 servidores.php
@@ -1,15 +1,14 @@
 {
   "user_info": {
-    "username": "950031",
-    "password": "ccwWkn",
-    "message": null,
+    "username": "952582b",
+    "password": "418724b",
+    "message": "LOUVADO SEJA DEUS...",
     "auth": 1,
     "status": "Active",
-    "exp_date": null,
+    "exp_date": "1918411525",
     "is_trial": "0",
-    "active_cons": 1,
-    "created_at": "1567789797",
-    "max_connections": "5",
+    "active_cons": "0",
+    "max_connections": "2",
     "allowed_output_formats": [
       "m3u8",
       "ts"
@@ -19,13 +18,12 @@
     "xui": true,
     "version": "1.5.5",
     "revision": 2,
-    "url": "mix34.ddns.net",
+    "url": "fontez.cc",
     "port": "80",
     "https_port": "443",
     "server_protocol": "http",
     "rtmp_port": "8880",
-    "timestamp_now": 1776385789,
-    "time_now": "2026-04-16 21:29:49",
+    "timestamp_now": 1776386163,
     "timezone": "America/Sao_Paulo"
   }
 }
