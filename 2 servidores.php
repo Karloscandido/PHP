36dc052afe096d942a38704e24fac0574df98dc2
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-16 20:31:43 -0400

    Update 2 servidores.php

--- 2 servidores.php
+++ 2 servidores.php
@@ -28,30 +28,4 @@
     "time_now": "2026-04-16 21:29:49",
     "timezone": "America/Sao_Paulo"
   }
-}    "status": "Active",
-    "exp_date": null,
-    "is_trial": "0",
-    "active_cons": 0,
-    "max_connections": "300",
-    "allowed_output_formats": [
-      "m3u8",
-      "ts",
-      "rtmp"
-    ]
-  },
-  "server_info": {
-    "xui": true,
-    "version": "1.5.5",
-    "revision": 2,
-    "url": "mix34.ddns.net",
-    "url": "mix34.ddns.net",
-    "url": "mix34.ddns.net",
-    "url": "mix34.ddns.net",
-    "port": "80",
-    "https_port": "443",
-    "server_protocol": "http",
-    "rtmp_port": "8880",
-    "timestamp_now": 1774892165,
-    "timezone": "America/Sao_Paulo"
-  }
 }
