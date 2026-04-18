8ce89bb4dfc14de3b2b6e2084e1c2f9e44e39ea5
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-18 17:59:48 -0400

    Update 300 user player_api.php

--- 300 user player_api.php
+++ 300 user player_api.php
@@ -1,30 +1,30 @@
 {
   "user_info": {
-    "username": "950031",
-    "password": "ccwWkn",
+    "username": "818347",
+    "password": "dRQENM",
     "message": null,
     "auth": 1,
     "status": "Active",
     "exp_date": null,
     "is_trial": "0",
-    "active_cons": 1,
-    "created_at": "1567789797",
-    "max_connections": "5",
+    "active_cons": 0,
+    "max_connections": "300",
     "allowed_output_formats": [
       "m3u8",
-      "ts"
+      "ts",
+      "rtmp"
     ]
   },
   "server_info": {
     "xui": true,
     "version": "1.5.5",
     "revision": 2,
-    "url": "mix34.ddns.net",
+    "url": "znationplay.com",
     "port": "80",
     "https_port": "443",
     "server_protocol": "http",
     "rtmp_port": "8880",
-    "timestamp_now": 1776388311,
+    "timestamp_now": 1776549408,
     "timezone": "America/Sao_Paulo"
   }
 }
