5fa4b85bc398de72db8f3fc7be72bcaed054ab13
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-18 20:27:35 -0400

    Update 300 user player_api.php

--- 300 user player_api.php
+++ 300 user player_api.php
@@ -1,15 +1,15 @@
 {
   "user_info": {
-    "username": "tanaka",
-    "password": "302718",
-    "message": "Welcome to XUI.one",
+    "username": "752311234",
+    "password": "642658119",
+    "message": "Bem vindo THUNDER PLAY",
     "auth": 1,
     "status": "Active",
-    "exp_date": "1838672159",
+    "exp_date": "1780887599",
     "is_trial": "0",
-    "active_cons": "1",
-    "created_at": "1773123561",
-    "max_connections": "6",
+    "active_cons": "0",
+    "created_at": "1776110246",
+    "max_connections": "3",
     "allowed_output_formats": [
       "m3u8",
       "ts",
@@ -20,13 +20,13 @@
     "xui": true,
     "version": "1.5.13",
     "revision": null,
-    "url": "topflx.online",
+    "url": "acxll.shop",
     "port": "80",
     "https_port": "443",
     "server_protocol": "http",
     "rtmp_port": "8880",
-    "timestamp_now": 1776558140,
-    "time_now": "2026-04-18 20:22:20",
-    "timezone": "America/Manaus"
+    "timestamp_now": 1776558405,
+    "time_now": "2026-04-18 21:26:45",
+    "timezone": "America/Sao_Paulo"
   }
 }
