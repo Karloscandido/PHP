7708a47146b3ce552931a8409232e80bc7891858
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-18 20:19:53 -0400

    Update 2 servidores.php

--- 2 servidores.php
+++ 2 servidores.php
@@ -1,30 +1,29 @@
 {
   "user_info": {
-    "username": "818347",
-    "password": "dRQENM",
+    "username": "950031",
+    "password": "ccwWkn",
     "message": null,
     "auth": 1,
     "status": "Active",
     "exp_date": null,
     "is_trial": "0",
     "active_cons": 0,
-    "max_connections": "300",
+    "max_connections": "50",
     "allowed_output_formats": [
       "m3u8",
-      "ts",
-      "rtmp"
+      "ts"
     ]
   },
   "server_info": {
     "xui": true,
     "version": "1.5.5",
     "revision": 2,
-    "url": "znationplay.com",
+    "url": "mix34.ddns.net",
     "port": "80",
     "https_port": "443",
     "server_protocol": "http",
     "rtmp_port": "8880",
-    "timestamp_now": 1776549408,
+    "timestamp_now": 1776557722,
     "timezone": "America/Sao_Paulo"
   }
 }
